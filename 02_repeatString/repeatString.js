c019abac34ac498cfe63abf44682dc54ce314be1
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-29 18:08:04 +0200

    Finish exercise 02
    Use of the ternary operator when declaring a variable

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let output =
+        num < 0 ? "ERROR" :
+            num === 0 ? "" :
+                string;
+    for (let index = 1; index < num; index++) {
+        output += string;
+    }
+    return output
 };
 
 // Do not edit below this line
