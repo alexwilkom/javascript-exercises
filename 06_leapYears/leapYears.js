2cd0c9b4fe0e3312d5cca0419bfddf8226624bac
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-30 10:49:18 +0200

    Finish exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // if year is divisible by 4
+    // and divisible by 400
+    // and not divisible by 100
 
+    // (year % 4 === 0) &&
+    // (year % 400 === 0 || !(year % 100 === 0))
+    return (year % 4 === 0) && (year % 400 === 0 || !(year % 100 === 0))
 };
 
 // Do not edit below this line
