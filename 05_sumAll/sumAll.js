feca974a44d0d10b3a615a3414220b11d92b1be4
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-30 10:27:32 +0200

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+    // Validation
+    if ((from < 0 || to < 0) ||
+        (!Number.isInteger(from) || !Number.isInteger(to))) {
+        return "ERROR";
+    }
 
+    // Logic
+    let sum = 0;
+    const max = Math.max(from, to);
+    const min = Math.min(from, to);
+
+    for (let number = min; number <= max; number++) {
+        sum += number
+    }
+
+    return sum
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
