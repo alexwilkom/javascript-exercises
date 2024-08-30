cb339c4e1b4fd16c74b6d1c8998ee55e501f3f3f
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-30 11:14:44 +0200

    Finish exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const { camelCase } = require("case-anything");
+
+const convertToCelsius = function (degree) {
+  // formula: °C = ((°F-32)(5)) / 9
+  const celsius = ((degree - 32) * 5) / 9;
+
+  if (!Number.isInteger(celsius)) {
+    return Number(celsius.toFixed(1));
+  }
+
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  // formula: °F = (9/5)(°C) + 32
+  const fahrenheit = ((9 / 5) * degree + 32);
+
+  if (!Number.isInteger(fahrenheit)) {
+    return Number(fahrenheit.toFixed(1));
+  }
+
+  return fahrenheit
 };
 
 // Do not edit below this line
